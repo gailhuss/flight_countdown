784ec169e85152153d5d749154c7857b322237c9
Gail Huss <gailhusscode@gmail.com>
2019-12-24 23:39:00 +0200

    UI and timer for countdown

--- src/app/src/main/java/com/example/flight_countdown/MainActivity.kt
+++ src/app/src/main/java/com/example/flight_countdown/MainActivity.kt
@@ -1,12 +1,57 @@
 package com.example.flight_countdown
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.CountDownTimer
+import android.view.Window
+import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_main.*
+import java.time.LocalDateTime
+
 
 class MainActivity : AppCompatActivity() {
+    private val calculator = TimeCalc(LocalDateTime.parse("2020-01-19T10:25:00"))
+    private lateinit var timer: CountDownTimer
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        requestWindowFeature(Window.FEATURE_NO_TITLE)
         setContentView(R.layout.activity_main)
+        initTimer()
+        setTime()
+    }
+
+    override fun onStart() {
+        super.onStart()
+
+        initTimer()
+    }
+
+    private fun initTimer()
+    {
+        timer = object : CountDownTimer(15*60*1000, 1000) {
+            override fun onFinish() {
+            }
+
+            override fun onTick(millisUntilFinished: Long) {
+                setTime()
+            }
+        }.start()
+    }
+
+    private fun setTime()
+    {
+        val diff = calculator.getDiff(LocalDateTime.now())
+        daysView.text = diff[0].toString()
+
+        hoursView.text = diff[1].toString()
+
+        minutesView.text = diff[2].toString()
+
+        secondsView.text = diff[3].toString()
+    }
+
+    override fun onPause() {
+        super.onPause()
+        timer.cancel()
     }
 }
