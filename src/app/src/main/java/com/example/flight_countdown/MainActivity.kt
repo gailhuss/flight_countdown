d9185db8b6aa18055058808e8c31d16de37297cf
Gail Huss <gailhusscode@gmail.com>
2019-12-30 00:24:46 +0200

    Daily background change. Close #2

--- src/app/src/main/java/com/example/flight_countdown/MainActivity.kt
+++ src/app/src/main/java/com/example/flight_countdown/MainActivity.kt
@@ -11,19 +11,24 @@ import java.time.LocalDateTime
 class MainActivity : AppCompatActivity() {
     private val calculator = TimeCalc(LocalDateTime.parse("2020-01-19T10:25:00"))
     private lateinit var timer: CountDownTimer
+    private val totalColors = 8
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         requestWindowFeature(Window.FEATURE_NO_TITLE)
         setContentView(R.layout.activity_main)
-        initTimer()
-        setTime()
     }
 
     override fun onStart() {
         super.onStart()
 
         initTimer()
+        setTime()
+    }
+
+    override fun onPause() {
+        super.onPause()
+        timer.cancel()
     }
 
     private fun initTimer()
@@ -48,10 +53,27 @@ class MainActivity : AppCompatActivity() {
         minutesView.text = diff[2].toString()
 
         secondsView.text = diff[3].toString()
+
+        setBackColor(diff[0].toInt())
     }
 
-    override fun onPause() {
-        super.onPause()
-        timer.cancel()
+    private fun setBackColor(daysDiff: Int) {
+        if(daysDiff != 0) {
+            var backColor =
+                when (daysDiff % totalColors) {
+                    0 -> R.color.background_day_0
+                    1 -> R.color.background_day_1
+                    2 -> R.color.background_day_2
+                    3 -> R.color.background_day_3
+                    4 -> R.color.background_day_4
+                    5 -> R.color.background_day_5
+                    6 -> R.color.background_day_6
+                    7 -> R.color.background_day_7
+                    else -> 0
+                }
+
+            rootLayout.setBackgroundColor(getColor(backColor))
+        }
     }
+
 }
